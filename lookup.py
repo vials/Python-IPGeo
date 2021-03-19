a6c599c815cdda19904cdbe0c5dafacafa6f6e4d
Inc <35619461+vials@users.noreply.github.com>
2021-03-19 18:08:14 -0500

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -5,8 +5,10 @@ import requests
 # To retrieve a key, sign up on: 'https://ipstack.com' to retrieve a free API key. Once you have it, edit the Token variable with your updated key.
 # To run: 'python3 lookup.py'
 
+Token = "EnterTokenHere"
+
 IP = input("Enter Target IP: ")
 
-r = requests.get("http://api.ipstack.com/1.3.3.7?access_key=d343f129a1e16f630632ba017655305c")
+r = requests.get("http://api.ipstack.com/1.3.3.7?access_key={}".format(Token))
 for index, element in enumerate(r.json()):
     print("{}: {}".format(element, r.json()[element]))
